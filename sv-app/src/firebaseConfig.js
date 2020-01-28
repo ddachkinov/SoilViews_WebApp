3b4d6fc515af78216d719e7bfe0a6004f012f731
ddachkinov <dimitar.dachkinov@dxc.com>
2020-01-28 06:36:48 +0200

    Setting the Firebase

---
+++ sv-app/src/firebaseConfig.js
@@ -0,0 +1,9 @@
+  export default {
+    apiKey: "AIzaSyA1hJMwM_cIrsCYieYvKN7XlyEXX4BHKUk",
+    authDomain: "soilviews-6209f.firebaseapp.com",
+    databaseURL: "https://soilviews-6209f.firebaseio.com",
+    projectId: "soilviews-6209f",
+    storageBucket: "soilviews-6209f.appspot.com",
+    messagingSenderId: "540877995304",
+    appId: "1:540877995304:web:720c54f946cc30e71be41d"
+  };
