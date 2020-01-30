39f5972652f3c78d9da465026f9e7d46110d88ef
ddachkinov <dimitar.dachkinov@dxc.com>
2020-01-30 14:33:46 +0200

    Initial Commit

--- sv-app/src/App.js
+++ sv-app/src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 import withFirebaseAuth from 'react-with-firebase-auth'
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseConfig from './firebaseConfig';
+import logo from './logo.svg';
+import './App.css';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-
 
 class App extends Component {
   render() {
@@ -17,14 +16,14 @@ class App extends Component {
       signInWithGoogle,
     } = this.props;
 
-  return (
-    <div className="App">
+    return (
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           {
             user
               ? <p>Hello, {user.displayName}</p>
-              : <p>Please sign in.</p>
+              : <p>Welcome to SoilViews!</p>
           }
 
           {
@@ -34,7 +33,8 @@ class App extends Component {
           }
         </header>
       </div>
-  );
+    );
+  }
 }
 
 const firebaseAppAuth = firebaseApp.auth();
